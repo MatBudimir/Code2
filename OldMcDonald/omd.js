4be50d18ca2b20625059c918a87b8710185d3a25
MatBudimir <dotbudi@gmail.com>
2024-10-23 22:10:44 +0200

    fin

--- OldMcDonald/omd.js
+++ OldMcDonald/omd.js
@@ -1,6 +1,7 @@
 "use strict";
 var OldMcDonald;
 (function (OldMcDonald) {
+    // Classes
     class Animal {
         constructor(_name, _type, _food, _appetite, _sound) {
             this.set(_name, _type, _food, _appetite, _sound);
@@ -12,9 +13,37 @@ var OldMcDonald;
             this.appetite = _appetite;
             this.sound = _sound;
         }
-        eat(_type, _food, _appetite) {
+        sing() {
+            const nameplate = document.querySelector("#name");
+            nameplate.innerHTML = `${this.name}`;
+            const lyrics = document.querySelector("#lyrics");
+            lyrics.innerHTML = `Old MacDonald had a farm. E-I-E-I-O. <br>
+            And on that farm he had a ${this.type}. E-I-E-I-O. <br>
+            With an ${this.sound} ${this.sound} here. <br>
+            And an ${this.sound} ${this.sound} there. <br>
+            Here an ${this.sound}. <br>
+            There an ${this.sound}. <br>
+            Everywhere an ${this.sound} ${this.sound}. <br>
+            Old MacDonald had a farm. E-I-E-I-O.`;
         }
-        sing(_name, _sound) {
+        eat(_stockpile) {
+            for (let i = 0; i < stockpile.length; i++) {
+                if (stockpile[i].type === this.food && stockpile[i].amount >= this.appetite) {
+                    stockpile[i].amount -= this.appetite;
+                    const foodtext = document.querySelector(`#food`);
+                    foodtext.innerHTML = `${this.name} the ${this.type} ate ${this.appetite} ${this.food}.`;
+                    document.querySelector(`#f${i}`).innerHTML = stockpile[i].consumption();
+                }
+                else if (stockpile[i].type === this.food && stockpile[i].amount < this.appetite) {
+                    const foodtext = document.querySelector(`#food`);
+                    foodtext.innerHTML = `Not enough ${this.food} left for ${this.name} to eat!`;
+                    document.querySelector(`#f${i}`).innerHTML = stockpile[i].consumption();
+                }
+            }
+        }
+        update(_stockpile) {
+            this.eat(_stockpile);
+            this.sing();
         }
     }
     class Food {
@@ -25,20 +54,40 @@ var OldMcDonald;
             this.type = _type;
             this.amount = _amount;
         }
-        consumtion(_type, _amount) {
+        consumption() {
+            return `${this.type}: ${this.amount}`;
         }
     }
+    // Game Initial Setup
     const animals = [];
-    animals.push(new Animal("Barky", "Dog", "Bones", 2, "Woof"));
-    animals.push(new Animal("Snowball", "Cat", "Fish", 1, "Meow"));
-    animals.push(new Animal("Kong", "Monkey", "Bananas", 7, "U-u A-a"));
-    animals.push(new Animal("Stripey", "Zebra", "Apples", 5, "Hiyah"));
-    animals.push(new Animal("Schnitzel", "Pig", "Cabbage", 3, "Oink"));
+    animals.push(new Animal("Barky", "Dog", "Bones", 2, "woof"));
+    animals.push(new Animal("Snowball", "Cat", "Fish", 1, "meow"));
+    animals.push(new Animal("Kong", "Monkey", "Bananas", 7, "u-u a-a"));
+    animals.push(new Animal("Stripey", "Zebra", "Apples", 5, "hiyah"));
+    animals.push(new Animal("Schnitzel", "Pig", "Cabbages", 3, "oink"));
     const stockpile = [];
     stockpile.push(new Food("Bones", 24));
     stockpile.push(new Food("Fish", 10));
-    stockpile.push(new Food("Bananas", 18));
-    stockpile.push(new Food("Apples", 99));
+    stockpile.push(new Food("Bananas", 38));
+    stockpile.push(new Food("Apples", 89));
     stockpile.push(new Food("Cabbage", 19));
+    let i = 0;
+    let j = 1;
+    document.querySelector('#clock').innerHTML = "Day " + j;
+    for (let i = 0; i < stockpile.length; i++) {
+        document.querySelector(`#f${i}`).innerHTML = stockpile[i].consumption();
+    }
+    // Game Loop
+    const nextButton = document.querySelector("#nextButton");
+    nextButton.addEventListener("click", () => {
+        i++;
+        if (i >= animals.length) {
+            i = 0;
+            j++;
+        }
+        animals[i].update(stockpile);
+        document.querySelector('#clock').innerHTML = "Day " + j;
+    });
+    animals[i].update(stockpile);
 })(OldMcDonald || (OldMcDonald = {}));
 //# sourceMappingURL=omd.js.map
--- OldMcDonald/omd.ts
+++ OldMcDonald/omd.ts
@@ -1,17 +1,21 @@
-namespace OldMcDonald {
-
-    class Animal {
+namespace OldMcDonald
+{
+    // Classes
+    class Animal
+    {
         public name: string;
         public type: string;
         public food: string;
         public appetite: number;
         public sound: string;
 
-        public constructor(_name: string, _type: string, _food: string, _appetite: number, _sound: string) {
+        public constructor(_name: string, _type: string, _food: string, _appetite: number, _sound: string)
+        {
             this.set(_name, _type, _food, _appetite, _sound);
         }
-    
-        public set(_name: string, _type: string, _food: string, _appetite: number, _sound: string): void {
+
+        public set(_name: string, _type: string, _food: string, _appetite: number, _sound: string): void
+        {
             this.name = _name;
             this.type = _type;
             this.food = _food;
@@ -19,44 +23,108 @@ namespace OldMcDonald {
             this.sound = _sound;
         }
 
-        public eat(_type: string, _food: string, _appetite: number): void {
+        public sing(): void
+        {
+            const nameplate: HTMLDivElement = document.querySelector("#name")!;
+            nameplate.innerHTML = `${this.name}`;
 
+            const lyrics: HTMLDivElement = document.querySelector("#lyrics")!;
+            lyrics.innerHTML = `Old MacDonald had a farm. E-I-E-I-O. <br>
+            And on that farm he had a ${this.type}. E-I-E-I-O. <br>
+            With an ${this.sound} ${this.sound} here. <br>
+            And an ${this.sound} ${this.sound} there. <br>
+            Here an ${this.sound}. <br>
+            There an ${this.sound}. <br>
+            Everywhere an ${this.sound} ${this.sound}. <br>
+            Old MacDonald had a farm. E-I-E-I-O.`;
         }
 
-        public sing(_name: string, _sound: string): void {
+        public eat(_stockpile: Array<Food>): void
+        {
+            for (let i: number = 0; i < stockpile.length; i++)
+            {
+                if (stockpile[i].type === this.food && stockpile[i].amount >= this.appetite)
+                {
+                    stockpile[i].amount -= this.appetite;
+                    const foodtext: HTMLDivElement = document.querySelector(`#food`)!;
+                    foodtext.innerHTML = `${this.name} the ${this.type} ate ${this.appetite} ${this.food}.`;
+                    document.querySelector(`#f${i}`)!.innerHTML = stockpile[i].consumption();
 
+                } else if (stockpile[i].type === this.food && stockpile[i].amount < this.appetite)
+                {
+                    const foodtext: HTMLDivElement = document.querySelector(`#food`)!;
+                    foodtext.innerHTML = `Not enough ${this.food} left for ${this.name} to eat!`;
+                    document.querySelector(`#f${i}`)!.innerHTML = stockpile[i].consumption();
+                }
+            }
+        }
+
+        public update(_stockpile: Array<Food>): void
+        {
+            this.eat(_stockpile);
+            this.sing();
         }
     }
 
-    class Food {
+    class Food
+    {
         type: string;
         amount: number;
 
-        public constructor(_type: string, _amount: number) {
+        public constructor(_type: string, _amount: number)
+        {
             this.set(_type, _amount);
         }
 
-        public set(_type: string, _amount: number): void {
+        public set(_type: string, _amount: number): void
+        {
             this.type = _type;
             this.amount = _amount;
         }
 
-        consumtion(_type: string, _amount: number): void {
-
+        consumption(): string
+        {
+            return `${this.type}: ${this.amount}`;
         }
     }
-    
+
+    // Game Initial Setup
     const animals: Array<Animal> = [];
-    animals.push(new Animal("Barky", "Dog", "Bones", 2, "Woof"));
-    animals.push(new Animal("Snowball", "Cat", "Fish", 1, "Meow"));
-    animals.push(new Animal("Kong", "Monkey", "Bananas", 7, "U-u A-a"));
-    animals.push(new Animal("Stripey", "Zebra", "Apples", 5, "Hiyah"));
-    animals.push(new Animal("Schnitzel", "Pig", "Cabbage", 3, "Oink"))
+    animals.push(new Animal("Barky", "Dog", "Bones", 2, "woof"));
+    animals.push(new Animal("Snowball", "Cat", "Fish", 1, "meow"));
+    animals.push(new Animal("Kong", "Monkey", "Bananas", 7, "u-u a-a"));
+    animals.push(new Animal("Stripey", "Zebra", "Apples", 5, "hiyah"));
+    animals.push(new Animal("Schnitzel", "Pig", "Cabbages", 3, "oink"));
 
     const stockpile: Array<Food> = [];
     stockpile.push(new Food("Bones", 24));
     stockpile.push(new Food("Fish", 10));
-    stockpile.push(new Food("Bananas", 18));
-    stockpile.push(new Food("Apples", 99));
+    stockpile.push(new Food("Bananas", 38));
+    stockpile.push(new Food("Apples", 89));
     stockpile.push(new Food("Cabbage", 19));
+
+    let i: number = 0;
+    let j: number = 1;
+
+    document.querySelector('#clock')!.innerHTML = "Day " + j;
+
+    for (let i:number = 0; i < stockpile.length; i++)
+    {
+        document.querySelector(`#f${i}`)!.innerHTML = stockpile[i].consumption();
+    }
+
+    // Game Loop
+    const nextButton: HTMLButtonElement = document.querySelector("#nextButton")!;
+    nextButton.addEventListener("click", () =>
+    {
+        i++;
+        if (i >= animals.length)
+        {
+            i = 0;
+            j++;
+        }
+        animals[i].update(stockpile);
+        document.querySelector('#clock')!.innerHTML = "Day " + j;
+    });
+    animals[i].update(stockpile);
 }
